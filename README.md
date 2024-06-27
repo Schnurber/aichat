8e4e7abfc0dd0931949ef5e30045b9c1dfc4b8f9
Schnurber <d.meiller@oth-aw.de>
2024-06-27 19:30:43 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# A ai chat Flet app
+
+A small and customisable app programmed in flet to chat with chatgpt via the openai interface. Easily customisable, clearly understandable.
+![AI Chat](screenshot.png)
+
+First add a file named .env with your Open-AI-Key!
+
+To run the app:
+
+`OPENAI_API_KEY=sk-proj-...`
+
+```
+pip install flet
+flet run 
+```
---
+++ main.py
@@ -0,0 +1,74 @@
+import flet as ft
+from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
+
+#customize first question and the role
+question = 'Who was Ada Lovelace?'
+role = 'You are William Shakespeare and you only speak in rhyme.'
+
+client = OpenAI()
+
+isAsking = False
+
+def main(page: ft.Page):
+    messages = []
+    tf = ft.TextField(value=question, expand=True, 
+                      autofocus=True, shift_enter=True,
+                      bgcolor=ft.colors.GREY_700,icon=ft.icons.WECHAT_OUTLINED)
+    lf = ft.ListView(controls=messages, auto_scroll=False, expand=True, reverse=True)
+    btt = ft.IconButton(icon=ft.icons.SEND_OUTLINED)
+    def ask(e):
+        global isAsking
+        if isAsking:
+            return
+        isAsking = True
+        btt.disabled = True
+        responseText = ''
+        txt = ft.Text(responseText, selectable=True)
+        question = tf.value
+        messages.insert(0,ft.Card(
+            content=ft.Container(padding=5,content=ft.Text(question, selectable=True)), 
+            color=ft.colors.BLUE_400, margin=ft.Margin(left=10,right=0, top=5, bottom=5)))
+        messages.insert(0,ft.Card(
+            content=ft.Container(padding=5,content=txt),
+            color=ft.colors.GREY_700, margin=ft.Margin(left=0,right=10, top=5, bottom=5)))
+        if len(messages) >= 100:
+            del messages[-2:] #if it is too long
+        stream = client.chat.completions.create(
+            model="gpt-4-turbo",
+            stream=True,
+            messages=[
+                {"role": "system", "content": role},
+                {"role": "user", "content": question}
+            ]
+        )
+        for chunk in stream:
+            msg = chunk.choices[0].delta
+            if msg.content is not None:
+                responseText += msg.content
+                txt.value = responseText
+                lf.scroll_to(0.0, duration=500)
+                page.update()
+        btt.disabled = False
+        page.update()
+        isAsking = False
+
+    btt.on_click = ask
+    tf.on_submit = ask
+    container= ft.Container(
+        expand=True,
+        content=ft.Column(
+            controls=[
+            lf,
+            ft.Row(
+                controls=[
+                tf,
+                btt
+            ])
+        ])
+    )
+
+    page.add(container)
+    
+ft.app(main)
