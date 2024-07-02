ad27786b6b7141d1cc191db2cbd322e698c84b3f
Schnurber <d.meiller@oth-aw.de>
2024-07-02 16:25:45 +0200

    self-talk

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #customize first question and the role
-question = 'Who was Ada Lovelace?'
-role = 'You are William Shakespeare and you only speak in rhyme.'
+question = 'Who are you?'
+role = 'You have a conversation and give a short answer. The last sentence is always a question'
 
-client = OpenAI()
+MAX_ROUNDS = 100
+
+numRounds = 0
+client1 = OpenAI()
+client2 = OpenAI()
+client = client1
 
 isAsking = False
 
@@ -17,56 +22,85 @@ def main(page: ft.Page):
                       autofocus=True, shift_enter=True,
                       bgcolor=ft.colors.GREY_700,icon=ft.icons.WECHAT_OUTLINED)
     lf = ft.ListView(controls=messages, auto_scroll=False, expand=True, reverse=True)
-    btt = ft.IconButton(icon=ft.icons.SEND_OUTLINED)
-    
+    btt = ft.IconButton(icon=ft.icons.PLAY_ARROW)
+    btt_stop = ft.IconButton(icon=ft.icons.STOP)
+    btt_stop.disabled = True
+
+    def getCard(txt, ab):
+        if ab:
+            return ft.Card(
+                content=ft.Container(padding=5,content=txt), 
+                color=ft.colors.BLUE_400, margin=ft.Margin(left=10,right=0, top=5, bottom=5))
+        else:
+            return ft.Card(
+                content=ft.Container(padding=5,content=txt),
+                color=ft.colors.GREY_700, margin=ft.Margin(left=0,right=10, top=5, bottom=5))
+        
     def ask(e):
-        global isAsking
+        global isAsking, numRounds, MAX_ROUNDS, client
         question = tf.value
+        numRounds = 0
         if isAsking or question == '':
             return
+        
         isAsking = True
         btt.disabled = True
-        responseText = ''
-    
-        tf.value = ''
-
-        messages.insert(0,ft.Card(
-            content=ft.Container(padding=5,content=ft.Text(question, selectable=True)), 
-            color=ft.colors.BLUE_400, margin=ft.Margin(left=10,right=0, top=5, bottom=5)))
+        btt_stop.disabled = False
+        quest = ft.Text(question, selectable=True)
+        messages.insert(0,getCard(quest,True))
         
-        txt = ft.Text(responseText, selectable=True)
-        messages.insert(0,ft.Card(
-            content=ft.Container(padding=5,content=txt),
-            color=ft.colors.GREY_700, margin=ft.Margin(left=0,right=10, top=5, bottom=5)))
-        if len(messages) >= 100:    
-            del messages[-2:] #if it is too long
-          
-        try:
-            stream = client.chat.completions.create(
-                model="gpt-4-turbo",
-                stream=True,
-                messages=[
-                    {"role": "system", "content": role},
-                    {"role": "user", "content": question}
-                ]
-            )
-            for chunk in stream:
-                msg = chunk.choices[0].delta
-                if msg.content is not None:
-                    responseText += msg.content
-                    txt.value = responseText
-                    lf.scroll_to(0.0, duration=500)
-                    page.update()
-        except:
-            txt.value = ' NO INTERNET CONNECTION!'
-            lf.scroll_to(0.0, duration=500)
+        while numRounds < MAX_ROUNDS:
+            numRounds += 1
+            responseText = ''
+            tf.value = ''
+            
+            txt = ft.Text(responseText, selectable=True)
+            messages.insert(0,getCard(txt, client == client2))
+            if len(messages) >= 100:    
+                del messages[-2:] #if it is too long
+            
+            try:
+                stream = client.chat.completions.create(
+                    model="gpt-4-turbo",
+                    stream=True,
+                    messages=[
+                        {"role": "system", "content": role},
+                        {"role": "user", "content": question}
+                    ]
+                )
+                for chunk in stream:
+                    msg = chunk.choices[0].delta
+                    if msg.content is not None:
+                        responseText += msg.content
+                        txt.value = responseText
+                        lf.scroll_to(0.0, duration=500)
+                        page.update()
+            except:
+                txt.value = ' NO INTERNET CONNECTION!'
+                lf.scroll_to(0.0, duration=500)
+            question = responseText
+
+            if client == client1:
+                client = client2
+            else:
+                client = client1
+
 
         btt.disabled = False
+        btt_stop.disabled = True
         page.update()
+        client = client1
         isAsking = False
+
+    def stop(e):
+        global numRounds
+        numRounds = MAX_ROUNDS
+        btt_stop.disabled = True
 
     btt.on_click = ask
     tf.on_submit = ask
+    btt_stop.on_click=stop
+
     container= ft.Container(
         expand=True,
         content=ft.Column(
@@ -75,7 +109,7 @@ def main(page: ft.Page):
             ft.Row(
                 controls=[
                 tf,
-                btt
+                btt, btt_stop
             ])
         ])
     )
