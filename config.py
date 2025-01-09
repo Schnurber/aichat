78d906bc971f253553798e3ef047b354640f55dd
Schnurber <d.meiller@oth-aw.de>
2025-01-09 11:58:35 +0100

    botcast

---
+++ config.py
@@ -0,0 +1,11 @@
+# Bit-Cast App
+#customize first question and the role
+conf = {
+'moderator' : {
+    'role' : 'Du bist der Moderator eines Podcasts. Der letzte Satz ist immer eine Frage.',
+    'first_question' :'Heute sprechen wir über die Geschichte des Computers. Was kannst Du mir darüber sagen?',
+},
+'specialist' : {
+    'role' : 'Du bist Historiker und kennst dich mit der Geschichte des Computers sehr gut aus. Du sprichst mit einem Moderator in einem Podcast. Kurze Antwort bitte.',
+},
+}
--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import flet as ft
+from config import conf
 from openai import OpenAI
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
 
-#customize first question and the role
-question = 'Wer bist Du?'
-role = 'Du führst eine Unterhaltung und gibst eine kurze Antwort. Der letzte Satz ist immer eine Frage'
+
+
 speech_file_path = Path(__file__).parent / "speech.mp3"
 MAX_ROUNDS = 100
 
 numRounds = 0
-client1 = OpenAI()
-client2 = OpenAI()
-client = client1
+moderator = OpenAI()
+specialist = OpenAI()
+client = moderator
 
 isAsking = False
 isPlaying = False
 
 def main(page: ft.Page):
     messages = []
-    tf = ft.TextField(value=question, expand=True, 
+    tf = ft.TextField(value=conf['moderator']['first_question'], expand=True, 
                       autofocus=True, shift_enter=True,
                       bgcolor=ft.colors.GREY_700,icon=ft.icons.WECHAT_OUTLINED)
     lf = ft.ListView(controls=messages, auto_scroll=False, expand=True, reverse=True)
@@ -31,12 +31,12 @@ def main(page: ft.Page):
     def getCard(txt, ab):
         if ab:
             return ft.Card(
-                content=ft.Container(padding=5,content=txt), 
-                color=ft.colors.BLUE_400, margin=ft.Margin(left=10,right=0, top=5, bottom=5))
-        else:
-            return ft.Card(
                 content=ft.Container(padding=5,content=txt),
                 color=ft.colors.GREY_700, margin=ft.Margin(left=0,right=10, top=5, bottom=5))
+        else:
+            return ft.Card(
+                content=ft.Container(padding=5,content=txt), 
+                color=ft.colors.BLUE_400, margin=ft.Margin(left=10,right=0, top=5, bottom=5))
         
     def ask(e):
         global isAsking, numRounds, MAX_ROUNDS, client, isPlaying
@@ -49,47 +49,48 @@ def main(page: ft.Page):
         btt.disabled = True
         btt_stop.disabled = False
         quest = ft.Text(question, selectable=True)
-        messages.insert(0,getCard(quest,True))
+        messages.insert(0,getCard(quest,False))
+        responseText = question
         
         while numRounds < MAX_ROUNDS:
             numRounds += 1
-            responseText = ''
             tf.value = ''
             
-            txt = ft.Text(responseText, selectable=True)
-            messages.insert(0,getCard(txt, client == client2))
-            if len(messages) >= 100:    
-                del messages[-2:] #if it is too long
-            
-            try:
-                stream = client.chat.completions.create(
-                    model="gpt-4-turbo",
-                    stream=True,
-                    messages=[
-                        {"role": "system", "content": role},
-                        {"role": "user", "content": question}
-                    ]
-                )
-                for chunk in stream:
-                    msg = chunk.choices[0].delta
-                    if msg.content is not None:
-                        responseText += msg.content
-                        txt.value = responseText
-                        lf.scroll_to(0.0, duration=500)
-                        page.update()
-            except:
-                txt.value = ' NO INTERNET CONNECTION!'
-                lf.scroll_to(0.0, duration=500)
+            if numRounds > 1:
+                responseText = ''
+                txt = ft.Text(responseText, selectable=True)
+                messages.insert(0,getCard(txt, client == specialist))
+                if len(messages) >= 100:    
+                    del messages[-2:] #if it is too long
+                try:
+                    stream = client.chat.completions.create(
+                        model="gpt-4-turbo",
+                        stream=True,
+                        messages=[
+                            {"role": "system", "content": conf['moderator']['role'] if client == moderator else conf['specialist']['role']},
+                            {"role": "user", "content": question}
+                        ]
+                    )
+                    for chunk in stream:
+                        msg = chunk.choices[0].delta
+                        if msg.content is not None:
+                            responseText += msg.content
+                            txt.value = responseText
+                            lf.scroll_to(0.0, duration=500)
+                            page.update()
+                except:
+                    txt.value = ' NO INTERNET CONNECTION!'
+                    lf.scroll_to(0.0, duration=500)
 
             with client.audio.speech.with_streaming_response.create(
                 model="tts-1",
-                voice="alloy" if client==client1 else "onyx",
+                voice="alloy" if client == moderator else "onyx",
                 input=responseText,
             ) as response:
                 response.stream_to_file(speech_file_path)
                 while (not response.is_closed):
                     pass
-
+           
             audio1 = ft.Audio( src=speech_file_path, autoplay=True)
             page.overlay.append(audio1)
             isPlaying = True
@@ -99,22 +100,24 @@ def main(page: ft.Page):
                     isPlaying = False
                     
             audio1.on_state_changed = ply
+            page.update()
             while isPlaying:
-                page.update()
+                pass
+            page.update()
             #remove last audio    
             page.overlay.pop()
             question = responseText
 
-            if client == client1:
-                client = client2
+            if client == moderator:
+                client = specialist
             else:
-                client = client1
+                client = moderator
 
 
         btt.disabled = False
         btt_stop.disabled = True
         page.update()
-        client = client1
+        client = moderator
         isAsking = False
 
     def stop(e):
